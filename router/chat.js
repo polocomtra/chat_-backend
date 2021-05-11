456bc1e2da7a650b9f9fee95b728deb12ae9a300
Tien Tai Nguyen <nguyentientai.3325@gmail.com>
2021-05-11 22:42:17 +0700

    done Chat CRUD

--- router/chat.js
+++ router/chat.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { index } = require('../controllers/chatController')
+const { index,create,messages, deleteChat } = require('../controllers/chatController')
 const router = express.Router();
 const {auth}=require('../middleware/auth')
 
 
 router.get('/',[auth], index)
+router.post('/create',[auth], create)
+router.post('/messages',[auth], messages)
+router.delete('/:id',[auth], deleteChat)
 
 
 module.exports = router;
